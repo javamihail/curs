e0577268be98f6c529e82bef11bdab06fb3eda92
Asus <ap_mihai@yahoo.com>
2021-09-08 15:11:35 +0300

    import

--- src/Data.java
+++ src/Data.java
@@ -4,7 +4,7 @@ public class Data {
         int myAge = 32;  // declaration
         float weight = 60.5f;
        double height =356.2;
-       long distance = 23423412323423431l;
+       long distance = 234234123223423431l;
        char gender = 'm';
        char test = 9;
        int test2 = (int) distance;
