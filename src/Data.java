e0755ff6d7106f8b2aaeef22199574aa91793d97
Asus <ap_mihai@yahoo.com>
2021-09-08 15:08:24 +0300

    import

---
+++ src/Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static void main(String[] args) {
+        // Definitre variabile, definim o variabila care arata varsta
+        int myAge = 32;  // declaration
+        float weight = 60.5f;
+       double height =356.2;
+       long distance = 23423412323423431l;
+       char gender = 'm';
+       char test = 9;
+       int test2 = (int) distance;
+        System.out.print(test2);
+    }
+}
